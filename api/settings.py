7e767c4dd937afbd3aead474dc7378ecdd92a325
Nihilist <77490318+Tuguldur666@users.noreply.github.com>
2025-05-04 17:26:41 +0800

    .

--- api/settings.py
+++ api/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'uploads')
+
 
 
 # Default primary key field type
